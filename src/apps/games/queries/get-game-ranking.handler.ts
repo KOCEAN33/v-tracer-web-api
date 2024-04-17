cfedcd0c705e9c8b6974bac530feb27ef9f437b0
kocean <dev@xanny.us>
2024-04-18 01:23:15 +0900

    add: game ranking endpoint

---
+++ src/apps/games/queries/get-game-ranking.handler.ts
@@ -0,0 +1,33 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+
+import { GetGameRankingQuery } from './get-game-ranking.query';
+import { GamesRepository } from '../repository/games.repository';
+
+@QueryHandler(GetGameRankingQuery)
+export class GetGameRankingHandler
+  implements IQueryHandler<GetGameRankingQuery>
+{
+  constructor(private readonly gamesRepository: GamesRepository) {}
+
+  async execute() {
+    const ranking = await this.gamesRepository.getPlayedGameRanking();
+
+    const rankingDuration = ranking.map((ranking) => {
+      return {
+        gameId: ranking.game_id,
+        gameTitle: ranking.title,
+        duration: this.converTimeToHours(ranking.total_duration),
+      };
+    });
+
+    return {
+      message: 'success',
+      ranking: rankingDuration,
+    };
+  }
+
+  private converTimeToHours(time: number): number {
+    const hours = time / 3600;
+    return Number(hours.toFixed(1));
+  }
+}
---
+++ src/apps/games/queries/get-game-ranking.query.ts
@@ -0,0 +1,5 @@
+import { IQuery } from '@nestjs/cqrs';
+
+export class GetGameRankingQuery implements IQuery {
+  constructor() {}
+}
